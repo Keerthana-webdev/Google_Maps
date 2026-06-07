8434a47bae58c3025ddf24bca966285c5accca99
keerthweb-1519 <yashodhakeerthi19@gmail.com>
2026-06-07 09:54:01 +0530

    described each step using comments

---
+++ google_maps.py
@@ -0,0 +1,36 @@
+import webbrowser #importing required library
+
+#defined required functions
+def map1(latitude, longitude):
+    url = f"https://www.google.com/maps?q={latitude},{longitude}"
+    print("Opening:", url)
+    webbrowser.open(url)
+    print("You are being redirected to Google Maps")
+
+def map2(address):
+    url = f"https://www.google.com/maps/search/{address}"
+    print("Opening:", url)
+    webbrowser.open(url)
+    print("You are being redirected to Google Maps")
+
+#question output
+print("""
+Select any option:
+    1. Address
+    2. Latitude and Longitude
+""")
+
+#program ends with user input
+selection = int(input(">> "))
+
+if selection == 1:
+    address = input("Give the address >> ")
+    map2(address)
+
+elif selection == 2:
+    latitude = input("Latitude >> ")
+    longitude = input("Longitude >> ")
+    map1(latitude, longitude)
+
+else:
+    print("That was a wrong input. Try again!")
